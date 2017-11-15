10e8d6d9d18dc53360dd67fb8460a43c8fd1e4ca
sahana.srinivasa <sahana.srinivasa@INBLR1L-133716.corp.ad.cginet>
2017-11-15 21:04:04 +0530

    adding few   classes

---
+++ com.project1/src/test/java/geberic/BaseTest.java
@@ -0,0 +1,35 @@
+package geberic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public abstract class BaseTest implements AutoConst{
+	public WebDriver driver;
+
+static
+{
+
+	System.setProperty(CHROME_KEY,CHROME_VALUE);
+	System.setProperty(GECKO_KEY, GECKO_VALUE);
+}	
+@BeforeMethod
+public void openApplication()
+{
+	
+	driver = new ChromeDriver();
+	driver.get(APPURL);
+}
+@AfterMethod
+public void closeApplication()
+{
+	driver.quit();
+}
+@Test
+public void Message()
+{
+	System.out.println("Application opend succefully");
+}
+}
